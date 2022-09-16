5ec253ea7cfe1115c204768ed9eabf731e86cac6
rphldbm <rphlldbm@gmail.com>
2022-09-16 14:06:39 -0300

    Dia 4 do workshop

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def chiclete(request):
+    return render(request, template_name='blog/home.html')
+
+def jujuba(request):
+    html = "<html><body><h1>Tome jujuba!</h1></body></html>"
+    return render(html)
